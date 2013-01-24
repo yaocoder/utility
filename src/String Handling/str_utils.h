2337ac5a140212a8780b02084cdee1363f9e3f58
yaocoder <yaocoder@gmail.com>
2013-01-24 14:59:33 +0800

    StringHandling utils

---
+++ src/String Handling/str_utils.h
@@ -0,0 +1,119 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+namespace str_utils
+{
+	inline std::string trimLeft(const std::string& str, const std::string& token)
+	{
+		std::string t = str;
+		t.erase(0, t.find_first_not_of(token));
+		return t;
+	}
+
+	inline std::string trimRight(const std::string& str, const std::string& token)
+	{
+		std::string t = str;
+		t.erase(t.find_last_not_of(token) + 1);
+		return t;
+	}
+
+	inline std::string trim(const std::string& str, const std::string& token)
+	{
+		std::string t = str;
+		t.erase(0, t.find_first_not_of(token));
+		t.erase(t.find_last_not_of(token) + 1);
+		return t;
+	}
+
+	inline std::string toLower(const std::string& str)
+	{
+		std::string t = str;
+		transform(t.begin(), t.end(), t.begin(), tolower);
+		return t;
+	}
+
+	inline std::string toUpper(const std::string& str)
+	{
+		std::string t = str;
+		transform(t.begin(), t.end(), t.begin(), toupper);
+		return t;
+	}
+
+	inline bool	startsWith(const std::string& str, const std::string& substr)
+	{
+		return str.find(substr) == 0;
+	}
+
+	inline bool endsWith(const std::string& str, const std::string& substr)
+	{
+		return str.rfind(substr) == (str.length() - substr.length());
+	}
+
+	inline bool equalsIgnoreCase(const std::string& str1, const std::string& str2) 
+	{
+		return toLower(str1) == toLower(str2);
+	}
+
+	template<class T> std::string toString(const T& value)
+	{
+		std::ostringstream oss;
+		oss << value;
+		return oss.str();
+	}
+
+	template<class T> void string2T(const std::string& str, T& value)
+	{	
+		std::istringstream iss(str);
+		iss >> value;
+		return ;
+	}
+
+	/*
+	warn: strtok is not thread-safe;
+	*/
+	inline std::vector<std::string> splitByDelimiter(const char* str,const char* delimiter)
+	{
+		char* token = strtok(const_cast<char*>(str),delimiter);
+
+		std::vector<std::string> result;
+
+		while(token != NULL)
+		{
+			result.push_back(token);
+			token = strtok(NULL,delimiter);
+		}
+		return result;
+	}
+
+	inline std::string joinStringByToken(const std::vector<std::string>&vec, const std::string& token)
+	{
+		std::string str;
+		for(unsigned int i = 0; i < vec.size(); ++i)
+		{
+			str = str + vec.at(i) + token;
+		}
+
+		return str;
+	}
+
+
+#ifdef BOOST
+	static std::vector<std::string> splitString(const std::string& str, const std::string& flag)
+	{
+		std::vector<std::string> vec_temp;
+		boost::algorithm::split(vec_temp, str, boost::algorithm::is_any_of(flag));
+		return vec_temp;
+	}
+
+	static std::string replaceString(const std::string& str,
+		const std::string& flag,
+		const std::string& replaceFlag)
+	{
+		boost::regex expressionReplace(flag);
+		std::string strTemp = boost::regex_replace(	str.c_str(), expressionReplace, replaceFlag);
+		return strTemp
+	}
+#endif
+
+}
