5d16bd44079f34aef8e35e4a08d714adf2697fdf
永远 <yaocoder@gmail.com>
2013-07-03 17:58:16 +0800

    Update RunbatchTest.bat

--- script/RunbatchTest.bat
+++ script/RunbatchTest.bat
@@ -4,8 +4,8 @@
  set /a b=b+1
  start UnitTest.exe
 SET SLEEP=ping 127.0.0.1 -n
-����%SLEEP% 30 > nul
+　　%SLEEP% 30 > nul
  if %b%==200 exit
  goto:a
  
- rem 30  间隔秒数 200 运行实例个数 
+  rem 30  间隔秒数 200 执行次数
